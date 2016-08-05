1e5a8038b92f1754704e70ca5df7dfe04edad121
Sven Lauterbach <sven.lauterbach@if-blueprint.de>
2016-08-05 16:05:30 +0200

    Added RelayCommand

--- App1/ViewModels/MainViewModel.cs
+++ App1/ViewModels/MainViewModel.cs
@@ -13,15 +13,19 @@ namespace App1.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        private string _firstname;
-        private string _lastname;
+        public MainViewModel()
+        {
+            InsertPersonCommand = new RelayCommand(InsertPerson);
+        }
         
+        private string _firstname;        
         public string Firstname
         {
             get { return _firstname; }
             set { Set(ref _firstname, value); }
         }
-        
+
+        private string _lastname;
         public string Lastname
         {
             get { return _lastname; }
@@ -29,12 +33,18 @@ namespace App1.ViewModels
         }
 
         private ObservableCollection<Person> _persons = new ObservableCollection<Person>();
-
         public ObservableCollection<Person> Persons
         {
             get { return _persons; }
         }
-        
+
+        private ICommand _insertPersonCommand;
+        public ICommand InsertPersonCommand
+        {
+            get { return _insertPersonCommand; }
+            set { Set(ref _insertPersonCommand, value); }
+        }
+
         public void InsertPerson()
         {
             Persons.Add(new Person
